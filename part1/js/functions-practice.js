f15fcce6cc7742db551289fd0633fce514e22616
Kibret Woldemichael <kwoldemichael@sdsu.edu>
2020-10-18 19:00:00 -0700

    Part 1 modulus complete

--- part1/js/functions-practice.js
+++ part1/js/functions-practice.js
@@ -1,22 +1,26 @@
 //STEP 1
 function halfNumber(num) {
-    var half = num/2;
+    var half = num / 2;
     window.console.log("Half of " + num + " is " + half + ".");
     return half;
 }
 //STEP 2
 function squareNumber(num) {
-    var square = num*num;
+    var square = num * num;
     window.console.log("The result of squaring the number " + num + " is " + square + ".");
     return square;
 }
 //STEP 3
 function percentOf(num1, num2) {
-    var percent = (num1/num2) * 100;
+    var percent = (num1 / num2) * 100;
     window.console.log(num1 + " is " + percent + "% of " + num2 + ".");
     return percent;
 }
-percentOf(4, 16);
 //STEP 4
-
+function findModulus(num1, num2) {
+    var mod = num2 % num1;
+    window.console.log(mod + " is the modulus of " + num1 + " and " + num2 + ".");
+    return mod;
+} 
+findModulus(4, 10);
 //STEP 5
