39646bddfad26b910c2855acfcdadb3af85264c3
Kibret Woldemichael <kwoldemichael@sdsu.edu>
2020-10-18 21:55:29 -0700

    Dom practice complete

--- part5/js/dom-practice.js
+++ part5/js/dom-practice.js
@@ -1,12 +1,33 @@
 /*eslint-env browser*/
 
 //STEP 1
-
+function clicked() {
+    window.alert("I have been clicked");
+}
 //STEP 2
-
+var btnTwo = document.getElementById("two");
+btnTwo.onclick = function () {
+    window.alert("I have been clicked");
+}
 //STEP 3
+var btnTwo = document.getElementById("three");
+btnTwo.addEventListener("click", showAlert);
 
+function showAlert() {
+    window.alert("I have been clicked");
+}
 //STEP 4
-
+var btnFour = document.getElementById("four");
+btnFour.addEventListener("click", function () {
+    window.alert("I have been clicked");
+});
 //STEP 5
-
+// The other code must be commented out for this to work
+function five() {
+    "use strict";
+    var btnFive = document.getElementById("five");
+    btnFive.addEventListener("click", function () {
+        window.alert("I have been clicked");
+    });
+}
+window.addEventListener("load", five);
