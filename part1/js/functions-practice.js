bc6cb00172f09becebe3d7e5756a0249d6af42f5
Kibret Woldemichael <kwoldemichael@sdsu.edu>
2020-10-18 18:42:49 -0700

    Part 1 half number complete

---
+++ part1/js/functions-practice.js
@@ -0,0 +1,13 @@
+//STEP 1
+function halfNumber(num) {
+    var half = num/2;
+    window.console.log("Half of " + num + " is " + half + ".");
+    return half;
+}
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
